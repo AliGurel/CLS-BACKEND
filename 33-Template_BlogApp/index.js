f75b20421d9e06e2706533fe9e6b1cce0863bf0a
AliGurel <aliigurel@gmail.com>
2024-04-27 11:57:29 +0300

    template

---
+++ 33-Template_BlogApp/index.js
@@ -0,0 +1,91 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+/*
+ * $ npm init -y
+ * $ npm i express dotenv express-async-errors
+ * $ npm i mongoose
+*/
+
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+/* ------------------------------------------------------- */
+//TEMPLATE
+
+//açma kapama etiketi ejs de default olarak <% ... %> idi. 
+//Biz bunun yerine farklı bir sembol kullanmak isteyebiliriz.
+//Değiştirme yöntemi - 1;
+// const ejs = require('ejs')
+// ejs.delimiter = '#' // artık delimeter % yerine # oldu. => yani <# ... #>
+// ejs.openDelimiter = '{' // açma işareti de artık { oldu. yani {# ... #>
+// ejs.closeDelimiter = '}' // kapama etiketini de } oldu.  yani {# ... #} oldu artık
+
+
+app.set('view engine', 'ejs')
+//Değiştirme yöntemi - 2 (kısa yol);
+app.set('view options', {
+    //delimiter: '%',
+    openDelimiter : '{',
+    closaDelimiter : '}'
+})
+
+app.set('views', './public')
+
+
+
+/* ------------------------------------------------------- */
+// SessionCookies:
+// http://expressjs.com/en/resources/middleware/cookie-session.html
+// https://www.npmjs.com/package/cookie-session
+//* $ npm i cookie-session
+const session = require("cookie-session")
+app.use(session({ secret: process.env.SECRET_KEY || 'secret_keys_for_cookies' }))
+/* ------------------------------------------------------- */
+// Accept json data & convert to object:
+app.use(express.json())
+
+// Connect to MongoDB with Mongoose:
+require('./src/dbConnection')
+
+// Searching&Sorting&Pagination:
+app.use(require('./src/middlewares/findSearchSortPage'))
+
+// HomePage:
+app.all('/*', (req, res) => {
+    // yukarıdaki yıldız gelen herşeyi kabul et demek
+    //direkt ana sayfa url si gelince /views a yönlendirsin demek
+    // if (req.originalUrl == '/') {
+    //     res.redirect('/views')
+    // }
+    // if (req.originalUrl.startsWith('/api')) {
+    //     res.send({
+    //         error: false,
+    //         message: 'WELCOME TO BLOG APP'
+    //     })
+    // }else {
+    //     res.send('<h1> WELCOME TO BLOG APP </h1>')
+    // }
+    res.send('<h1> WELCOME TO BLOG APP </h1>')
+})
+
+// Routes: // Bunlar VIEWS yanei Template dir
+app.use('/views/user', require('./src/routes/views/userRoute'))
+app.use('/views/blog', require('./src/routes/views/blogRoute'))
+
+// Routes: // Bunlar API dir
+app.use('/api/user', require('./src/routes/api/userRoute'))
+app.use('/api/blog', require('./src/routes/api/blogRoute'))
+
+/* ------------------------------------------------------- */
+// Synchronization:
+// require('./src/sync')()
+
+// errorHandler:
+app.use(require('./src/errorHandler'))
+
+app.listen(PORT, () => console.log('Running: http://127.0.0.1:' + PORT))
---
+++ 33-Template_BlogApp/src/routes/views/userRoute.js
@@ -0,0 +1,36 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const router = require('express').Router()
+
+const { User } = require('../../controllers/views/userController')
+
+// ------------------------------------------
+// User
+// ------------------------------------------
+
+//işin içine template girdiğinde get, post ayrılmaz direkt all yapılır
+
+// Login/logout:
+router.all('/login', User.login)
+router.all('/logout', User.logout)
+
+
+// router.route('/')
+//     .get(User.list)
+//     .post(User.create)
+
+// router.route('/:userId')
+//     .get(User.read)
+//     .put(User.update)
+//     .delete(User.delete)
+
+router.all('/', User.list)
+router.all('/create', User.create)
+router.all('/:id', User.read)
+router.all('/:id/update', User.update)
+router.all('/:id/delete', User.delete)
+
+module.exports = router
