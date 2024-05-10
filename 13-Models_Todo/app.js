cfc005a45cea993bdf70409c5b82782763cfc3e1
AliGurel <aliigurel@gmail.com>
2024-05-10 22:05:50 +0300

    statuscode

--- 13-Models_Todo/app.js
+++ 13-Models_Todo/app.js
@@ -26,7 +26,8 @@ require('express-async-errors')
 
 const { Sequelize, DataTypes} = require('sequelize')
 // Sequelize bir motordur
-const sequelize = new Sequelize('sqlite:./db.sqlite3')
+const sequelize = new Sequelize('sqlite:./db.sqlite3') //sqlite: olması lazım, ./db.sqlite3 = yolu
+// db.sqlite3 diye isim vermek şart değil, uzantısı sqşite3 olması iyi olur
 
 // define methodu sequelize modeli olusturur:
 // her bir model, veritabaninda bir tabloya denk gelir.
@@ -55,12 +56,12 @@ const Todo = sequelize.define('todos',{
     priority: { // -1 Low, 0: Normal, 1: High
         type: DataTypes.TINYINT,
         allowNull: false,
-        default: 0
+        defaultValue: 0
     },
     isDone: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
-        default: false
+        defaultValue: false
     }
     // diğerlerine (createdAt ve upatedAt)gerek yok kendi tanımlıyor oto olarak
 
@@ -133,6 +134,53 @@ router.get('/:id', async (req,res)=>{
         result: data
     })
 })
+
+//UPDATE TODO
+router.put('/:id', async (req,res)=>{
+    //const data = await Todo.update({ ...neyle güncellicez...}, { ... nereyi güncellicez...}})
+    const data = await Todo.update(req.body, { where: {id: req.params.id}})
+
+    res.status(202).send({
+        error: false,
+        message : 'Updated',
+        body: req.body,// gönderdiğim yeni veriyi gösterir
+        result: data, // güncellenen veri sayısını verir
+        new: await Todo.findByPk(req.params.id) // güncellendikten sonraki veriyi gösterir
+    })
+})
+
+// DELETE TODO
+router.delete('/:id', async (req,res)=>{
+
+    //const data = await Todo.destroy({ ...hangi kaydı silicez...})
+    const data = await Todo.destroy({ where: { id: req.params.id}})
+    console.log(data);// silinen kayıt sayısını konsola verir
+
+    //204 NO CONTENT => bu statuscode ekrana çıktı vermeyebilir, içeriği sildiği için,
+    // o nedenle ya 204 dışında bir statuscode vericez veya bunu if kontrolüyle yapıcaz aşağıdaki gibi
+    // res.status(204).send({
+    //     error: false,
+    //     message: 'Deleted',
+    //     result: data
+    // })
+
+    if (data>0){ //silme gerçekleşti ise
+        //res.status(204).send()// aşağıdaki de aynı işi yapar, send olmazsa istek döngüde kalır
+        //sadece statuscodu çıktı vermek için aşağıdaki yazılır
+        res.sendStatus(204)
+    } else{ // silme gerekleşmedi ise
+        // res.status(404).send({
+        //     error: true,
+        //     result: data
+        // })
+        //yukarıdaki gibi de yapabiliriz, aşağıdaki gibi de
+        // eror handlera da havale edebiliriz, 
+        res.errorStatusCode = 404
+        throw new Error('Not Found')
+    }
+
+})
+
 
 app.use(router)
 
